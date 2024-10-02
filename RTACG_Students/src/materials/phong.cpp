21737085ed889a4c621902adbab8e2ed8c5857e3
MAranich <114926980+MAranich@users.noreply.github.com>
2024-10-02 20:01:30 +0200

    phong working reverse

--- RTACG_Students/src/materials/phong.cpp
+++ RTACG_Students/src/materials/phong.cpp
@@ -12,16 +12,16 @@ rho_d(Kd_), Ks(Ks_), alpha(alpha_){}
 Vector3D Phong::getReflectance(const Vector3D& n, const Vector3D& wo, const Vector3D& wi) const {
 
     //FILL(...)
-    // double pi = 3.14159265358979323846;
+    double pi = 3.14159265358979323846; // 355/113
 
 
-    Vector3D wr = n * 2 * n.dot(wi) - wi;
-    double dot_p2 = pow((wo * wr).length(), alpha);
-    Vector3D specular = Ks * dot_p2;
+    Vector3D reflected_ray = n * (2 * n.dot(wi)) - wi;
+    double dot_p2 = pow(wo.dot(reflected_ray), alpha);
+    Vector3D specular = Ks * dot_p2; 
     // Vector3D fs = rho_d / pi + specular;
     // return fs;
 
-    return specular; 
+    return specular + rho_d / pi;
 
 };
 
--- RTACG_Students/src/shaders/whittedintegrator.cpp
+++ RTACG_Students/src/shaders/whittedintegrator.cpp
@@ -21,20 +21,24 @@ Vector3D whittedintegrator::computeColor(const Ray & r,
 		return Shader::bgColor;
 	}
 
-
+	//we have a collision
 	Vector3D collision_point = intersection.itsPoint; 
+	// accumulator for the final result
 	Vector3D final_color = Vector3D(0, 0, 0); 
-	// w0 
-	Vector3D camera_ray = (intersection.itsPoint - r.o).normalized(); 
+	// w0 (in reality = -camera ray)
+	Vector3D camera_ray = (r.o - intersection.itsPoint).normalized();
+	
 
+	//printf("%d", lsList.size()); 
 	for (const LightSource* light : lsList) {
 
-		Vector3D pos = light->sampleLightPosition();
-		Vector3D shadow_dir = collision_point - pos;
+		Vector3D light_pos = light->sampleLightPosition(); 
+		Vector3D shadow_dir = collision_point - light_pos;
 		double shadow_length = shadow_dir.length(); 
+		Vector3D shadow_dir_norm = shadow_dir / shadow_length;
 
-		Ray light_ray = Ray(collision_point, shadow_dir / shadow_length, (size_t)0, Epsilon, shadow_length);
-		bool has_collided = Utils::hasIntersection(light_ray, objList);
+		Ray shadow_ray = Ray(collision_point, shadow_dir_norm, (size_t)0, Epsilon, shadow_length);
+		bool has_collided = Utils::hasIntersection(shadow_ray, objList);
 
 		if (has_collided) {
 			continue; 
@@ -42,26 +46,33 @@ Vector3D whittedintegrator::computeColor(const Ray & r,
 
 		const Material& mat = intersection.shape->getMaterial();
 		Vector3D normal = intersection.normal;
+		normal = normal.normalized(); 
 
 
 		Vector3D diffuse = mat.getDiffuseReflectance();
-		Vector3D specular = mat.getReflectance(normal, shadow_dir.normalized(), camera_ray);
+		Vector3D specular = mat.getReflectance(normal, camera_ray, shadow_dir_norm);
 
 		Vector3D color = diffuse + specular;
-		double coef = normal.dot(camera_ray);
+		double coef = normal.dot(shadow_dir_norm);
+
+		if (coef == 0) printf("COEF 0\n");
 		if (coef <= 0) {
 			// coef = max(coef, 0)
 			coef = 0;
 		}
 
 
+
 		color = color * coef;
+		Vector3D increment = color * light->getIntensity(); 
+
+		//printf("%f\n", increment.length()); 
 
 
-		final_color = final_color + color * light->getIntensity();
+		final_color = final_color + increment;
 	}
 
-	Vector3D ambient = Vector3D(0.5, 0.0, 0.0);
+	Vector3D ambient = Vector3D(0.1, 0.0, 0.0);
 	final_color = final_color + ambient;
 
 	return final_color;
