724f70797786192745354f6eae17f0e010e4b934
martapurti <marta.purti01@estudiant.upf.edu>
2024-10-03 14:34:23 +0200

    Mirror 2

--- RTACG_Students/src/materials/mirror.cpp
+++ RTACG_Students/src/materials/mirror.cpp
@@ -12,12 +12,19 @@ rho_d(Kd_), Ks(Ks_), alpha(alpha_){}
 Vector3D Mirror::getReflectance(const Vector3D& n, const Vector3D& wo, const Vector3D& wi) const {
 
     //FILL(...)
-    double pi = 3.14159265358979323846; // 355/113
+    //double pi = 3.14159265358979323846; // 355/113
+    bool perfect_specular = hasSpecular();
+    bool gloss_diffuse = hasDiffuseOrGlossy();
+    Vector3D wo;
+    Vector3D wr;
+    Vector3D n;
 
 
-    Vector3D reflected_ray = n * (2 * n.dot(wi)) - wi;
-    double dot_p2 = pow(wo.dot(reflected_ray), alpha);
-    Vector3D specular = Ks * dot_p2; 
+
+    
+    //Vector3D reflected_ray = n * (2 * n.dot(wi)) - wi;
+    //double dot_p2 = pow(wo.dot(reflected_ray), alpha);
+    //Vector3D specular = Ks * dot_p2; 
     // Vector3D fs = rho_d / pi + specular;
     // return fs;
 
